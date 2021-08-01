e7e808fdd20a7774a57d688c3fcb70293a743ae6
Родион <rodion843@gmail.com>
2021-08-01 19:26:16 +0300

    доделал по ТЗ, навести красоту осталось

--- src/Panorama/Panorama.cpp
+++ src/Panorama/Panorama.cpp
@@ -6,27 +6,51 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/video/video.hpp>
-#include <thread>
-#include <mutex>
-cv::Rect Box;
+#include <string>
+
+cv::Rect ROI_Selector;
 cv::Mat ResultImage;
-cv::Mat frame;
-std::mutex imshowMutex;
-std::thread ImageGrabber;
+cv::Mat OneCameraFrame;
+
+cv::Point Mouse_origin;
+bool selectObject = false;
+bool DrawRoiedImage;
+cv::Rect selection;
+
+const std::string WindowName = "source";
+
+static bool showSelections() {
+    cv::rectangle(ResultImage, selection, cv::Scalar(255, 255, 255), 5);
+    cv::imshow(WindowName, ResultImage);
+  return true;
+}
+
 void onMouse(int event, int x, int y, int, void *)
 {
+
   switch (event) {
   case cv::MouseEventTypes::EVENT_LBUTTONDOWN:
-    ImageGrabber = std::thread([&]() {
-        imshowMutex.lock();
-        std::cout << "thread 2\n";
-        cv::Mat img = cv::imread("C:/Untitled.png");
-        Box = cv::selectROI("roi sel", img);
-        cv::Mat cropped = img(Box);
-        cropped.copyTo(img(cv::Rect(0, 300, Box.width, Box.height)));
-        imshowMutex.unlock();
-    });
+    Mouse_origin = cv::Point(x, y);
+    ResultImage = cv::Scalar(0, 0, 0);
+    selectObject = true;
+    DrawRoiedImage = false;
+    showSelections();
     break;
+  case cv::MouseEventTypes::EVENT_LBUTTONUP:
+    DrawRoiedImage = true;
+    selectObject = false;
+    
+    break;
+  }
+  if (selectObject) {
+    selection.x = MIN(x, Mouse_origin.x);
+    selection.y = MIN(y, Mouse_origin.y);
+    selection.width = std::abs(x - Mouse_origin.x) +1;
+    selection.height = std::abs(y - Mouse_origin.y) +1;
+    selection &= cv::Rect(0, 0, OneCameraFrame.cols, OneCameraFrame.rows);
+    if (selection.width > 0 && selection.height > 0) {
+      ROI_Selector = selection;
+    }
   }
 }
 int main(int /*argc*/, char * /*argv*/[])
@@ -50,12 +74,12 @@ int main(int /*argc*/, char * /*argv*/[])
     formatConverter.OutputPixelFormat = Pylon::PixelType_BGR8packed;
     Pylon::CPylonImage pylonImage;
 
-    cv::namedWindow("soruce", cv::WindowFlags::WINDOW_AUTOSIZE);
-    cv::setMouseCallback("soruce", onMouse, 0);
+    cv::namedWindow(WindowName, cv::WindowFlags::WINDOW_NORMAL);
+    cv::setMouseCallback(WindowName, onMouse, 0);
     //if (camera.GrabOne(5000, ptrGrabResult)) {
       //frame = cv::Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC1, pylonImage.GetBuffer());
     //}
-    ResultImage = cv::Mat::zeros(cv::Size(/*frame.cols*/ 2500, /*frame.rows * 2*/ 600), CV_8UC1);
+    ResultImage = cv::Mat::zeros(cv::Size(/*frame.cols*/ 6000, /*frame.rows * 2*/ 1000), CV_8UC1);
 
     camera.StartGrabbing();
 
@@ -64,11 +88,16 @@ int main(int /*argc*/, char * /*argv*/[])
       camera.RetrieveResult(5000, ptrGrabResult, Pylon::TimeoutHandling_ThrowException);
       if (ptrGrabResult->GrabSucceeded()) {
         formatConverter.Convert(pylonImage, ptrGrabResult);
-        frame = cv::Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC1, (uint8_t *)pylonImage.GetBuffer());
-        imshowMutex.lock();
-        frame.copyTo(ResultImage(cv::Rect{ 0, 0, frame.cols, frame.rows }));
-        cv::imshow("soruce", ResultImage);
-        imshowMutex.unlock();
+        OneCameraFrame = cv::Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC1, (uint8_t *)pylonImage.GetBuffer());
+        OneCameraFrame.copyTo(ResultImage(cv::Rect{ 0, 0, OneCameraFrame.cols, OneCameraFrame.rows }));
+        if (!selection.empty()) {
+          cv::rectangle(ResultImage, selection, cv::Scalar(255, 255, 255), 5);
+        }
+        if (DrawRoiedImage) {
+          cv::Mat roied_img = OneCameraFrame(cv::Rect{ 400, 40, 600, 80 });
+          OneCameraFrame(ROI_Selector).copyTo(ResultImage(cv::Rect{ 0, 256 + 20, ROI_Selector.width, ROI_Selector.height}));
+        }
+        cv::imshow(WindowName, ResultImage);
         cv::waitKey(1);
       } else {
         std::cout << "Error: " << std::hex << ptrGrabResult->GetErrorCode() << std::dec << " " << ptrGrabResult->GetErrorDescription() << std::endl;
@@ -76,7 +105,6 @@ int main(int /*argc*/, char * /*argv*/[])
     }
 
 
-    ImageGrabber.join();
   } catch (const Pylon::GenericException &e) {
     // Error handling.
     std::cerr << "An exception occurred." << std::endl
@@ -86,6 +114,5 @@ int main(int /*argc*/, char * /*argv*/[])
   }
   // Comment the following two lines to disable waiting on exit.
   // Releases all pylon resources.
-  
   return 0;
 }
